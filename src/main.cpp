31a4bb04110591a5a1412d3f50f5f779a0833f91
Anton Levchenko <anton.levchenko.ext@wsa.com>
2025-02-13 13:25:14 +0100

    Compiles, 3 states added to the example

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,24 @@
 #include "fsm.h"
 #include <iostream>
+#include <functional>
 
 enum 
 {
     STATE_INITIAL,
     STATE_IDLE,
-    STATE_ACTIVE
+    STATE_ACTIVE,
+    NUMBER_OF_STATES
 
 };
 
 class CApplication
 {    
-    CFsm::CState states[] = {
-        {CApplication::OnInitialExit(),   CApplication::OnInitialExit(),  "State Initial" },
-
-    };
+    std::array<CFsm::CState, NUMBER_OF_STATES> states = 
+    {{
+        { [this]() { OnInitialEnter(); }, [this]() { OnInitialExit(); },   "State Initial"  },
+        { [this]() { OnIdleEnter(); }, [this]() { OnIdleExit(); },   "State Idle"  },
+        { [this]() { OnActiveEnter(); }, [this]() { OnActiveExit(); },   "State Idle"  }        
+    }};
 
     public:
     CApplication();
@@ -22,6 +26,8 @@ class CApplication
     void OnIdleExit() { std::cout << "OnIdleExit() \n"; }
     void OnInitialEnter() { std::cout << "OnInitialEnter() \n"; }
     void OnInitialExit() { std::cout << "OnInitialExit() \n"; }
+    void OnActiveEnter() { std::cout << "OnActiveEnter() \n"; }
+    void OnActiveExit() { std::cout << "OnActiveExit() \n"; }    
     void Init();
     void Run();
 
@@ -30,15 +36,18 @@ class CApplication
 
 };
 
-CApplication:CApplication() : 
-    m_Fsm(states)
+CApplication::CApplication() : 
+    m_Fsm(states.data())  // Pass pointer to the state array
 {
-
-
 }
 
+void CApplication::Init()
+{
+}
 
-
+void CApplication::Run()
+{
+}
 
 int main() 
 {
